f9b0fff3cedcd2a0148dff2079a82df2c5d8b65a
Sylvain Combes <combes.sylvain@gmail.com>
2016-06-07 17:23:20 +0200

    Drag with hammer.js

--- build/jquery.images-compare.js
+++ build/jquery.images-compare.js
@@ -1,111 +1,177 @@
-(function ($) {
+;(function ($, window, document, undefined) {
 
-    $.fn.imagesCompare = function (options) {
-        //noinspection JSDuplicatedDeclaration
-        var options = $.extend({
+    var pluginName = 'imagesCompare',
+        defaults = {
             initVisibleRatio: 0.5,
-            mouseMove: true,
-            click: true,
+            interactionMode: "drag", // "drag", "mousemove", "click"
             precision: 2
+        };
 
-        }, options);
-
+    // Our object, using revealing module pattern
+    function ImagesCompare(element, options) {
+        element = $(element);
+        options = $.extend({}, defaults, options);
         options.roundFactor = parseInt('1' + '0'.repeat(options.precision));
 
-        return this.each(function () {
+        this._name = pluginName;
+        var frontElement, backElement, dragHandle, frontInitialCssTransitionDuration = "333ms";
 
-            var container = $(this);
-            
-            /**
-             *
-             * @param value
-             * @return {*}
-             */
-            $.fn.imagesCompare.setValue = function (value) {
-                return setVisibleRatio(value);
-            };
+        init();
+        function init() {
+            updateDom();
+            patchSize();
+            initInteractions();
+        }
 
-            if (options.mouseMove) {
-                var lastMove = 0;
-                var eventThrottle = 1;
-                container.on('mousemove', function (event) {
-                    event.preventDefault();
-                    var now = Date.now();
-                    if (now > lastMove + eventThrottle) {
-                        lastMove = now;
-                        console.log(event);
-                        var ratio = (event.pageX - container.offset().left) / frontElement.width();
-                        console.log(ratio);
-                        setVisibleRatio(ratio);
-                    }
-                });
+        function initInteractions() {
+            options.interactionMode = options.interactionMode.toLowerCase();
 
-                container.on('mouseout', function (event) {
-                    console.log('mouseout');
-                    var ratio = (event.pageX - container.offset().left) / frontElement.width();
-                    if (ratio > 1) {
-                        ratio = 1;
-                    }
-                    if (ratio < 0) {
-                        ratio = 0;
-                    }
-                    setVisibleRatio(ratio);
-                });
+            if (options.interactionMode != "drag" && options.interactionMode != "mousemove" && options.interactionMode != "click") {
+                console.warn('No valid interactionMode found, valid values are "drag", "mousemove", "click"');
             }
 
-            if (options.click) {
-                container.on('click', function (event) {
-                    console.log(event.target);
-                    var ratio = (event.pageX - container.offset().left) / frontElement.width();
-                    console.log(ratio);
-                    setVisibleRatio(ratio);
-                });
+            switch (options.interactionMode) {
+                case "drag":
+                    if (typeof Hammer == 'undefined') {
+                        console.error('Please include the hammerjs library for drag support');
+                    }
+                    addDrag();
+                    break;
+                case "mousemove":
+                    addMouseMove();
+                    break;
+                case "click":
+                    addClick();
+                    break;
             }
+        }
 
-            container.append("<div class='images-compare-handle'></div>");
-            var slider = container.find(".images-compare-handle");
-            slider.append("<span class='images-compare-left-arrow'></span>");
-            slider.append("<span class='images-compare-right-arrow'></span>");
+        function addClick() {
+            console.log('click event listener');
+            element.on('click', function (event) {
+                var ratio = getElementRatio(event.pageX);
+                setVisibleRatio(ratio);
+            });
+        }
 
-            container.addClass('images-compare-container');
+        function addMouseMove() {
+            var lastMove = 0;
+            var eventThrottle = 1;
+            element.on('mousemove', function (event) {
+                event.preventDefault();
+                var now = Date.now();
+                if (now > lastMove + eventThrottle) {
+                    lastMove = now;
+                    var ratio = getElementRatio(event.pageX);
+                    setVisibleRatio(ratio);
+                }
+            });
 
-            var frontElement = container.find('> *:nth-child(1)');
-            var backElement = container.find('> *:nth-child(2)');
+            element.on('mouseout', function (event) {
+                var ratio = getElementRatio(event.pageX);
+                setVisibleRatio(ratio);
+            });
+        }
+
+        function addDrag() {
+            var hammertime = new Hammer(element[0]);
+            hammertime.get('pan').set({direction: Hammer.DIRECTION_HORIZONTAL});
+            hammertime.on('pan', function (event) {
+                var ratio = getElementRatio(event.srcEvent.pageX);
+                setVisibleRatio(ratio);
+            });
+
+            hammertime.on('panstart', function (event) {
+                frontElement.addClass('images-compare-notransition');
+            });
+
+            hammertime.on('panend', function (event) {
+                frontElement.removeClass('images-compare-notransition');
+            });
+        }
+
+        function updateDom() {
+            element.addClass('images-compare-container');
+
+            frontElement = element.find('> *:nth-child(1)');
+            backElement = element.find('> *:nth-child(2)');
+
             frontElement.addClass("images-compare-before");
             backElement.addClass("images-compare-after");
+        }
 
+        function buildDragHandle() {
+            element.append("<div class='images-compare-handle'></div>");
+            dragHandle = element.find(".images-compare-handle");
+            dragHandle.append("<span class='images-compare-left-arrow'></span>");
+            dragHandle.append("<span class='images-compare-right-arrow'></span>");
+        }
+
+        function patchSize() {
             var imgRef = backElement.find('img').first();
-            container.width(imgRef.width());
-            container.height(imgRef.height());
+            element.width(imgRef.width());
+            element.height(imgRef.height());
+        }
 
-            function setVisibleRatio(ratio) {
-                ratio = Math.round((ratio * options.roundFactor)) / options.roundFactor;
-                console.log(ratio);
-                var width = Math.round(frontElement.width() * ratio);
-                frontElement.css('clip', 'rect(0, ' + width + 'px, ' + frontElement.height() + 'px, 0)');
-                container.trigger({
-                    type: 'change',
-                    ratio: ratio,
-                    value: width
-                });
+        function getElementRatio(x) {
+            return roundRatio((x - element.offset().left) / frontElement.width());
+        }
+
+        function roundRatio(ratio) {
+            ratio = Math.round((ratio * options.roundFactor)) / options.roundFactor;
+            if (ratio > 1) {
+                ratio = 1;
             }
 
-            setVisibleRatio(options.initVisibleRatio);
-
-            // check if same sizes
-            if (backElement.width() != frontElement.width()) {
-                console.error("Images haven't the same width !");
-                console.log("Please change width of image " + frontElement.attr('src') + " to " + backElement.width() + "px");
-                frontElement.width(backElement.width());
-
+            if (ratio < 0) {
+                ratio = 0;
             }
 
-            if (backElement.height() != frontElement.height()) {
-                console.error("Images haven't the same height !");
-                console.log("Please change height of image " + frontElement.attr('src') + " to " + backElement.height() + "px");
-            }
+            return ratio;
 
+        }
+
+        function getRatioValue(ratio) {
+            ratio = Math.round((ratio * options.roundFactor)) / options.roundFactor;
+            return Math.round(frontElement.width() * ratio);
+        }
+
+        function setVisibleRatio(ratio) {
+            var width = getRatioValue(ratio);
+            frontElement.css('clip', 'rect(0, ' + width + 'px, ' + frontElement.height() + 'px, 0)');
+            // frontElement.width(width);
+            // frontElement.removeClass('images-compare-notransition');
+            element.trigger({
+                type: 'change',
+                ratio: ratio,
+                value: width
+            });
+        }
+
+        // public function declaration
+        // returning element to preserve chaining
+        return {
+            "setValue": function (ratio) {
+                setVisibleRatio(ratio);
+                return element;
+            },
+            "on": function (eventName, callback) {
+                element.on(eventName, callback);
+            },
+            "off": function (eventName, callback) {
+                element.off(eventName, callback);
+            }
+        };
+    }
+
+
+    $.fn.imagesCompare = function (userOptions) {
+        var options = $.extend(defaults, userOptions);
+        return this.each(function () {
+            if (!$.data(this, pluginName)) {
+                $.data(this, pluginName, new ImagesCompare(this, options));
+            }
         });
     };
 
-})(jQuery);
+})(jQuery, window, document);
